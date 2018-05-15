d3a5f1c71cca6b6c85a35a660f79de21fedb9d3b
u211-02 <u211-02@public.mpei.local>
2018-05-15 10:09:44 +0300

    добавил print in hex

--- main.cpp
+++ main.cpp
@@ -6,6 +6,28 @@ char nibble_to_hex(uint8_t i) {
     char digits[] = "0123456789abcdef";
     return digits[i];
 }
+void print_in_hex(uint8_t byte)
+{
+    cout << nibble_to_hex(byte >> 4);
+    cout<< nibble_to_hex(byte & 0b00001111);
+}
+const uint8_t*
+as_bytes(const void* data) {
+    return reinterpret_cast<const uint8_t*>(data);
+}
+void print_in_hex(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_hex(bytes[i]);
+
+        if ((i + 1) % 16 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
 int main()
 {
     assert(nibble_to_hex(0x0) == '0');
@@ -16,7 +38,7 @@ int main()
     assert(nibble_to_hex(0x5) == '5');
     assert(nibble_to_hex(0x6) == '6');
     assert(nibble_to_hex(0x7) == '7');
-    assert(nibble_to_hex(0x9) == '8');
+    assert(nibble_to_hex(0x8) == '8');
     assert(nibble_to_hex(0x9) == '9');
     assert(nibble_to_hex(0xa) == 'a');
     assert(nibble_to_hex(0xb) == 'b');
