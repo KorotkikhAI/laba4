0d70af26b7d435523438b8246d9877b9a7813b0b
u211-02 <u211-02@public.mpei.local>
2018-05-15 10:37:40 +0300

    добавил print in binary

--- main.cpp
+++ main.cpp
@@ -28,6 +28,31 @@ void print_in_hex(const void* data, size_t size) {
         }
     }
 }
+char bit_digit(uint8_t byte, uint8_t bit) {
+    if (byte & (0x1 << bit)) {
+        return '1';
+    }
+    return '0';
+}
+void print_in_binary(uint8_t byte) {
+    for (uint8_t bit = 7; bit > 0; bit--) {
+        cout << bit_digit(byte, bit);
+    }
+}
+void
+print_in_binary(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_binary(bytes[i]);
+
+        if ((i + 1) % 4 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
 int main()
 {
     assert(nibble_to_hex(0x0) == '0');
@@ -46,4 +71,11 @@ int main()
     assert(nibble_to_hex(0xd) == 'd');
     assert(nibble_to_hex(0xe) == 'e');
     assert(nibble_to_hex(0xf) == 'f');
+    uint16_t u32 = 0x42;
+    cout << "u32 bytes: ";
+    print_in_hex(&u32, sizeof(u32));
+    cout << '\n';
+    uint8_t u8 =3;
+    print_in_binary(&u8, sizeof(u8));
+    cout << '\n';
 }
